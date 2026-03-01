ddf57d6f3b3fc5caaa51c24a77534244c6c80b44
FinZhang <shininglight441@gmail.com>
2026-03-01 18:56:04 +0000

    try fix

---
+++ next/static/F5aYyXuICGQS0Yj7y4RJn/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Nurania//_next/:path+",
+        "destination": "/Nurania/next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
